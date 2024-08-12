351f17b94de7c62f237a3922ee6bc82f55a40cbe
Reece Moore <reecemoore@192.168.1.113>
2024-08-12 16:46:26 +0100

    Started on task 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,16 @@
 package org.example;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.*;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -32,33 +37,63 @@ public class Main {
 //        });
 
         //Task 4
-        CompletableFuture<String> futureNum1 = CompletableFuture.supplyAsync(() -> {
-            try {
-                Thread.sleep(8000);
-            } catch (InterruptedException ie) {
-                ie.printStackTrace();
-            }
-            return "Hello";
-        });
+//        CompletableFuture<String> futureNum1 = CompletableFuture.supplyAsync(() -> {
+//            try {
+//                Thread.sleep(8000);
+//            } catch (InterruptedException ie) {
+//                ie.printStackTrace();
+//            }
+//            return "Hello";
+//        });
+//
+//        CompletableFuture<String> futureNum2 = CompletableFuture.supplyAsync(() -> {
+//            try {
+//                Thread.sleep(10000);
+//            } catch (InterruptedException ie) {
+//                ie.printStackTrace();
+//            }
+//           return "World!";
+//        });
+//
+//        CompletableFuture<String> combinedFuture = futureNum1.thenCombine(futureNum2, (Hello, World) -> Hello + ", " + World);
+//        combinedFuture = combinedFuture.orTimeout(10000, TimeUnit.MILLISECONDS).exceptionally(ex -> {
+//            if(ex instanceof TimeoutException) {
+//                return "Combined Time exceeds 10 seconds";
+//           }
+//            return "An error has occurred";
+//        });
+//        System.out.println(combinedFuture.get());
 
-        CompletableFuture<String> futureNum2 = CompletableFuture.supplyAsync(() -> {
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException ie) {
-                ie.printStackTrace();
-            }
-           return "World!";
-        });
 
-        CompletableFuture<String> combinedFuture = futureNum1.thenCombine(futureNum2, (Hello, World) -> Hello + ", " + World);
-        combinedFuture = combinedFuture.orTimeout(10000, TimeUnit.MILLISECONDS).exceptionally(ex -> {
-            if(ex instanceof TimeoutException) {
-                return "Combined Time exceeds 10 seconds";
-           }
-            return "An error has has occurred";
-        });
-        System.out.println(combinedFuture.get());
 
+        String data = "85671 34262 92143 50984 24515 68356 77247 12348 56789 98760";
+
+        String[] numbersString = data.split(" ");
+
+        List<BigInteger> integerList = new ArrayList<>();
+        for (String number: numbersString) {
+            integerList.add(new BigInteger(number));
+        }
+
+        System.out.println(integerList);
+
+
+
+                for (BigInteger number : integerList) {
+                    CompletableFuture<BigInteger> numberFuture = CompletableFuture.supplyAsync(() -> {number)
+                }
+
+
+
+
+    }
+
+    private static BigInteger calculateFactorial(BigInteger num) {
+        BigInteger result = BigInteger.ONE;
+        for (BigInteger i = BigInteger.ONE; i.compareTo(num) <= 0; i = i.add(BigInteger.ONE)) {
+            result = result.multiply(i);
+        }
+        return result;
     }
 }
 
