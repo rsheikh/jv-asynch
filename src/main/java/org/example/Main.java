693c25063f0e62a5546cf89fa6d3b509837c14af
Reece Moore <reecemoore@192.168.1.113>
2024-08-12 11:43:29 +0100

    Completed task 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,58 @@
 package org.example;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        System.out.println("------");
-        CompletableFuture<Void> cf =
-        CompletableFuture.runAsync(() -> {
-            System.out.println("Starting Async Process");
+        //Task 1
+//        CompletableFuture<Void> cf =
+//                CompletableFuture.runAsync(() -> {
+//                    System.out.println("Starting Async Process");
+//                    try {
+//                        Thread.sleep(2000);
+//                    } catch (InterruptedException ie) {
+//                        ie.printStackTrace();
+//                    }
+//                    System.out.println("Hello world!");
+//                });
+
+        //Task 3
+//        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> "Hello");
+//        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> "World!");
+
+
+        CompletableFuture<Void> futureNum1 = CompletableFuture.runAsync(() -> {
             try {
-                Thread.sleep(2000);
+                Thread.sleep(3000);
             } catch (InterruptedException ie) {
                 ie.printStackTrace();
             }
-            System.out.println("Hello world!");
+            System.out.println("Hello");
         });
 
-        cf.join();
-        }
+        CompletableFuture<Void> futureNum2 = CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+            System.out.println("World!");
+        });
 
+        futureNum2.join();
+
+
+
+
+
+
+    }
 }
+
+
